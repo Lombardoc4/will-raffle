b2dd854b8b4284cbb1d56892ccdb986fc803fe3a
Cris Lom <bigc4997@gmail.com>
2022-10-19 16:54:16 -0400

    drawing

--- src/Drawing.jsx
+++ src/Drawing.jsx
@@ -3,29 +3,39 @@ import { API, graphqlOperation } from 'aws-amplify';
 import { RaffleEntry } from './models';
 
 
-import Logo from './the-rat-club.png'
+import Logo from './summer-festival-tour-lams.png'
 import './drawing.css'
 import { listRaffleEntries } from './graphql/queries';
 
 export default function Drawing() {
     const [entries, setEntries] = useState([]);
     const [winner, setWinner] = useState(' ');
+    const [nextToken, setNextToken] = useState(null);
 
     useEffect(() => {
         fetchEntries()
-    }, [])
+    }, [nextToken])
 
     async function fetchEntries() {
+        console.log('fetch')
         try {
-          const entryData = await API.graphql(graphqlOperation(listRaffleEntries, {limit: "1000"}))
+          const entryData = await API.graphql(graphqlOperation(listRaffleEntries, {limit: "1000", nextToken: nextToken}))
+          console.log('entryData', entryData.data.listRaffleEntries);
+
           const dbEntries = entryData.data.listRaffleEntries.items
-          setEntries(dbEntries)
+          setEntries([...entries, ...dbEntries])
+
+          if (entryData.data.listRaffleEntries.nextToken) {
+            setNextToken(entryData.data.listRaffleEntries.nextToken);
+          }
         } catch (err) { console.log('error fetching todos') }
       }
 
     const generateRandomWinner = () => {
         setWinner(entries[Math.floor(Math.random()*entries.length)])
     }
+
+    console.log('nextToken', nextToken);
 
     return (
         <div className='drawing'>
@@ -34,7 +44,7 @@ export default function Drawing() {
             <h2>{winner.name}</h2>
             <p style={{fontSize: '0.66rem'}}>{winner.id}</p>
 
-            <button onClick={() => generateRandomWinner()}>Generate Winner</button>
+            {/* <button onClick={() => generateRandomWinner()}>Generate Winner</button> */}
 
 
             <div className="entry-list">
