7bbb2251216d093a669d44e729f9e33578690cee
Cris Lom <bigc4997@gmail.com>
2022-12-24 14:44:52 -0500

    update meta fields

--- src/conf.js
+++ src/conf.js
@@ -3,6 +3,6 @@ export const conf = {
     raffle_id: 'Decemeber-2022',
     raffle_opening_date: 'December 23, 2022 13:00:00 GMT-04:00', // Format : August 19, 1975 23:15:30 GMT-04:00
     raffle_drawing_date: 'December 28, 2022 13:00:00 GMT-04:00',
-    raffle_item: 'Signed LP & EP Tour Lams',
+    raffle_item: 'Signed LP & EP Tour Lams & Signed Tour Poster',
     patreon_only: true
 }
